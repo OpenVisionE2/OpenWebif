826ffb81cdb3bc17ac07ff0fa62552e6614306e4
persianpros <persianpros@yahoo.com>
2020-04-15 17:26:53 +0430

    Fix typo

--- plugin/controllers/models/info.py
+++ plugin/controllers/models/info.py
@@ -261,7 +261,7 @@ def getInfo(session=None, need_fullinfo=False):
 	info['imagefilesystem'] = boxbranding.getImageFileSystem()
 	info['feedsurl'] = boxbranding.getFeedsUrl()
 	info['developername'] = boxbranding.getDeveloperName()
-	info['builddatestring'] = boxbranding.getBuildDateString()
+	info['builddatestring'] = about.getBuildDateString()
 	info['imagefpu'] = boxbranding.getImageFPU()
 	info['havemultilib'] = boxbranding.getHaveMultiLib()
 
