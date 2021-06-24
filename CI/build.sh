f56e33aa4c1c476e90826b4284af5adcbbf7a813
persianpros <persianpros@yahoo.com>
2021-06-24 18:14:36 +0430

    Get rid of Travis forever as it's not free anymore!
    What travis-.com is advertising is bullshit!
    
    It's not free anymore not even for open source and you need to contact and beg them!
    
    They will give you some free credits then it finishes again so switch to github actions as soon as possible!

--- CI/build.sh
+++ CI/build.sh
@@ -4,7 +4,7 @@
 # You're not allowed to remove my copyright or reuse this script without putting this header.
 
 setup_git() {
-  git config --global user.email "bot@openvision.tech"
+  git config --global user.email "bot@OpenWebif.com"
   git config --global user.name "Open Vision python bot"
 }
 
@@ -14,14 +14,11 @@ commit_files() {
   rm -rf *.pyo
   rm -rf *.mo
   git checkout master
-  ./chmod.sh
-  ./dos2unix.sh
-  ./PEP8.sh
-  ./futurize.sh
+  ./CI/PEP8.sh
 }
 
 upload_files() {
-  git remote add upstream https://${GH_TOKEN}@github.com/OpenVisionE2/OpenWebif.git > /dev/null 2>&1
+  git remote add upstream https://${GITHUB_TOKEN}@github.com/OpenVisionE2/OpenWebif.git > /dev/null 2>&1
   git push --quiet upstream master || echo "failed to push with error $?"
 }
 
